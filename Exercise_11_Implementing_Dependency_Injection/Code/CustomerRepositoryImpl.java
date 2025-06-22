c35a0ec94a56c73ee710e74b0fab873e93336a48
KatipallyHethoshna <hethoshnardyk2004@gmail.com>
2025-06-23 01:04:59 +0530

    Delete Exercise_11_Implementing_Dependency_Injection directory

--- Exercise_11_Implementing_Dependency_Injection/Code/CustomerRepositoryImpl.java
+++
@@ -1,7 +0,0 @@
-public class CustomerRepositoryImpl implements CustomerRepository {
-    @Override
-    public Customer findCustomerById(String id) {
-        // Dummy data simulation
-        return new Customer(id, "John Doe");
-    }
-}
