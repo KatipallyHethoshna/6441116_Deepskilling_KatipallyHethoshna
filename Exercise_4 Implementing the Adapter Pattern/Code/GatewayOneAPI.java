c46abfb2b725424a1b0b0f656bdd26011d4ad64d
KatipallyHethoshna <hethoshnardyk2004@gmail.com>
2025-06-23 01:06:57 +0530

    Delete Exercise_4 Implementing the Adapter Pattern directory

--- Exercise_4 Implementing the Adapter Pattern/Code/GatewayOneAPI.java
+++
@@ -1,5 +0,0 @@
-public class GatewayOneAPI {
-    public void makeTransaction(double amt) {
-        System.out.println("GatewayOne: Payment of Rs" + amt + " completed.");
-    }
-}
--- Exercise_4 Implementing the Adapter Pattern/Code/GatewayTwoService.java
+++
@@ -1,5 +0,0 @@
-public class GatewayTwoService {
-    public void executePayment(String currency, double value) {
-        System.out.println("GatewayTwo: Processed " + currency + " " + value);
-    }
-}
