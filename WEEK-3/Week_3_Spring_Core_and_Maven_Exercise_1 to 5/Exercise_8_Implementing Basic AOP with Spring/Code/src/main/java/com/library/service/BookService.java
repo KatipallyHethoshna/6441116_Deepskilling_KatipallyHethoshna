cba28df83800547eb7c15611f68dd0b6ce3b4973
KatipallyHethoshna <hethoshnardyk2004@gmail.com>
2025-07-06 16:17:03 +0530

    Add files via upload

---
+++ WEEK-3/Week_3_Spring_Core_and_Maven_Exercise_1 to 5/Exercise_8_Implementing Basic AOP with Spring/Code/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,11 @@
+package com.library.service;
+
+public class BookService {
+    public void issueBook() {
+        System.out.println("BookService: Book has been issued.");
+    }
+
+    public void returnBook() {
+        System.out.println("BookService: Book has been returned.");
+    }
+}
