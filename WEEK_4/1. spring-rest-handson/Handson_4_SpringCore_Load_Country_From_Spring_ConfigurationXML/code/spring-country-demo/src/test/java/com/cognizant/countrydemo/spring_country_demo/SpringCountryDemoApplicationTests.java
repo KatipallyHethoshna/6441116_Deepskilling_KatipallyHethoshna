0224b12bbd50c8d789e57cce9105b12f1ad7ea75
KatipallyHethoshna <hethoshnardyk2004@gmail.com>
2025-07-17 19:55:48 +0530

    Add files via upload

---
+++ WEEK_4/1. spring-rest-handson/Handson_4_SpringCore_Load_Country_From_Spring_ConfigurationXML/code/spring-country-demo/src/test/java/com/cognizant/countrydemo/spring_country_demo/SpringCountryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.countrydemo.spring_country_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCountryDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
